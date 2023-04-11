84df54ac22555f5f4962b6c5d99753f283499665
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-04-10 17:34:58 -0700

    further study

--- movies.js
+++ movies.js
@@ -5,11 +5,33 @@ $(".submit-btn").on("click", function(event) {
 
   const title = $(".form-control").eq(0).val();
   const rating = $(".form-control").eq(1).val();
-  const removeBtn = $("<button>Remove</button>").addClass("remove-btn")
+  const removeBtn = $("<button>Remove</button>").addClass("remove-btn");
 
+  if (rating < 0 || rating > 10) {
+    alert('Rating must be between 0 and 10.');
+    throw new Error('Rating must be between 0 and 10.');
+  }
+
+  if (title.length < 2) {
+    alert('Title must have at least 2 characters.');
+    throw new Error('Title must have at least 2 characters.');s
+  }
+
+  
   $(".display-area").append($("<p>").text(title))
   $(".display-area").append($("<p>").text(rating))
   $(".display-area").append(removeBtn)
+
+
+});
+
+$(".display-area").on("click", ".remove-btn", function(evt){
+  $(evt.target).prev().remove()
+  $(evt.target).prev().remove()
+  $(evt.target).remove()
+
+  // find the parent of remove button (div), .remove() the div
+})
 
   /*
   const title = $(".form-control").eq(0).val();
@@ -23,12 +45,3 @@ $(".submit-btn").on("click", function(event) {
 
   $(".display-area").append(submissionSpace)
   */
-});
-
-$(".display-area").on("click", ".remove-btn", function(evt){
-  $(evt.target).prev().remove()
-  $(evt.target).prev().remove()
-  $(evt.target).remove()
-
-  // find the parent of remove button (div), .remove() the div
-})
